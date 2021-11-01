d7562861c4902fd3dcf84fed4586fb7ab1a70b3b
corya <steve@corya.me>
2021-10-31 22:01:26 -0400

    explicit string

--- index.js
+++ index.js
@@ -95,8 +95,8 @@ if (!nkn.Wallet.verifyAddress (toAddress)) {
 
 // check for the existence of the receipt file (which should contain the tx or hash)
 const checkFile = argv.directory.endsWith ('/') ?
-  argv.directory  + FILENAME :
-  argv.directory + '/' + FILENAME;
+  '' + argv.directory + FILENAME :
+  '' + argv.directory + '/' + FILENAME;
 log.info ('Checking for file at', checkFile);
 try {
   fs.statSync (checkFile);
