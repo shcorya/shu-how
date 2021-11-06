d2bd3d644b53c992209f12406ff0594a4ba37f4f
corya <steve@corya.me>
2021-11-06 10:45:23 -0400

    new strategy

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const log = require ('console-log-level') ({
     return `[shu-how] ${dateFormat.format (new Date ())} [${level.toLowerCase ()}]`
   },
   level: process.argv.includes ('--debug') ? 'debug' : 'info'
-})
+});
 
 /*-----\
 | args |
@@ -23,13 +23,13 @@ log.debug ('process.argv:', process.argv);
 log.debug ('Parsing arguments');
 const argv = require ('minimist') (process.argv.slice (2), {
   default: {
-    to: '/nkn/data/wallet.json',
-    directory: '/nkn/data'
+    interval: 15,
+    minimum: 0
   }
 });
 log.debug ('argv:', argv);
 
-const reqArgs = ['amount', 'fee', 'from', 'pswdfile'];
+const reqArgs = ['amount', 'fee', 'wallet', 'pswdfile', 'service'];
 reqArgs.forEach (arg => {
   if (!Object.keys (argv).includes (arg)) {
     log.error ('Missing required argument:', arg);
@@ -40,96 +40,38 @@ reqArgs.forEach (arg => {
 /*-----\
 | Main |
 \-----*/
-// leave the program running for docker
-process.on ('SIGTERM', process.exit);
-
+const { exec, execSync } = require ('child_process');
 const fs = require ('fs');
-const nkn = require ('nkn-sdk');
 
-// load funded wallet
-log.debug ('Loading wallet at', argv.from);
-const walletJSON = fs.readFileSync (argv.from).toString ();
-log.debug ('wallet.json:', JSON.parse (walletJSON));
-log.debug ('Loading wallet password at', argv.pswdfile);
-const walletPswd = fs.readFileSync (argv.pswdfile).toString ().trim ();
-log.debug ('pswd:', walletPswd);
-const fromWallet = nkn.Wallet.fromJSON (
-  walletJSON, {
-    password: walletPswd
-});
+// read password
+log.debug ('Reading password');
+const password = fs.readFileSync (argv.pswdfile).toString ().trim ();
 
-// load address to fund
-const toAddress = JSON.parse (fs.readFileSync (argv.to)).Address;
-if (!nkn.Wallet.verifyAddress (toAddress)) {
-  log.error ('Could not find a valid \'Address\' property at', argv.to);
-  process.exit (1);
-}
-log.debug ('Found to address', toAddress);
+// check balance
+log.debug ('Checking balance');
+var balance = JSON.parse (
+  execSync (`nknc --ip ${argv.seed} wallet --name ${argv.wallet} --password ${password} --list balance`)
+).result.amount;
+log.info ('Found a balance of', balance, 'NKN');
 
-// check for the existence of the receipt file (which should contain the tx or hash)
-var checkFile = '';
-checkFile = checkFile + (argv.directory.endsWith ('/') ?
-  argv.directory + FILENAME :
-  argv.directory + '/' + FILENAME);
-log.info ('Checking for file at', checkFile);
-try {
-  fs.statSync (checkFile);
+// check nonce
+log.debug ('Checking nonce');
+var nonce = JSON.parse (
+  execSync (`nknc --ip ${argv.seed} wallet --name ${argv.wallet} --password ${password} --list nonce`)
+).results.nonce;
+log.info ('Found nonce', nonce);
 
-  // file found
-  log.info (FILENAME, 'successfully found');
 
-} catch (noFileError) {
+const discovery = setInterval (async function discover () {
+  const dnsEndpoint = `tasks.${argv.service}.`;
+  const addresses = (await dig ([dnsEndpoint]))['answer'].map (a => a['value']);
+  // each address will correspond to a public key and each address may correspond to an id
+  // for each address with a public key but no id an id needs to be generated
+  // id's must be generated one-at-a-time in order to properly track pending NKN balance
+  const statuses = await Promise.all (addresses.map (address => {
+    exec (`nknc --ip ${address} info --state`); 
+  }));
 
-  // file not found 
-  log.warn ('Could not find file at', checkFile);
-
-  // ensure sufficient balance
-  log.info ('Checking wallet balance');
-  nkn.Wallet.getBalance (fromWallet.address).then (balance => {
-    log.info ('Found ' + balance.toString () + ' NKN at init address ' + fromWallet.address);
-    
-    const fee = new nkn.Amount (argv.fee);
-    const amount = new nkn.Amount (argv.amount);
-
-    log.debug ('fee:', fee.toString ());
-    log.debug ('amount:', amount.toString ());
-    log.debug ('fee + amount:', fee.plus (amount).toString ());
-    log.debug ('balance - (fee + amount):', balance.minus (fee.plus (amount)));
-    log.debug ('!(balance.comparedTo (amount.plus (fee)) >= 0):', !(balance.comparedTo (amount.plus (fee)) >= 0));
-    
-    // check whether or not balance amount is sufficient
-    if (!(balance.comparedTo (amount.plus (fee)) >= 0)) {
-      // insufficient balance to initialize a new wallet
-      log.error ('Insufficient NKN balance to initialize a new node');
-      process.exit (1);      
-
-    } else {
-
-      // balance is sufficient
-      if (!argv.dry) {  
-        fromWallet.transferTo (toAddress, amount.toString (), {
-          fee: fee.toString ()
-        }).then (txOrHash => {
-
-          // transaction successfully submitted
-          fs.writeFileSync (checkFile, JSON.stringify (txOrHash));
-          log.info ('Transaction successfully submitted and saved');
-
-        }, txFailure => {
-          // failed to submit transaction
-          log.error ('Could not submit transaction to NKN blockchain');
-          process.exit (1);
-        });
-      } else {
-
-        // dry run 
-        log.info ('Sufficient balance found, skipping tx in dry run');
-      }
-    }
-
-  }, balanceFailure => {
-    // could not retrieve balance
-    log.error ('Could not retrieve balance for wallet ' + fromWallet.address);
-    process.exit (1);
-  });
-}
+  const keysToFund = statuses.filter (status => {
+    return status.error.code == -45022 }).map (status => status.error.publicKey);
+}, argv.interval * 1000);
